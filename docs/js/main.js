776a0b0d64665d97be1819d675107f1cfde50ed9
nobita4176 <nobita4176@gmail.com>
2017-12-23 01:18:56 +0900

    impl js (stub)

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,9 +1,33 @@
 (function() {
 	'use strict';
 
+	var cards;
 	document.addEventListener('DOMContentLoaded', function() {
-		fetch('./furuyoni_cards.json').then(function(cards) {
+		fetch(
+			'./furuyoni_cards.json'
+		).then(function(res) {
+			cards = res.json();
 			console.log(cards);
 		});
+	});
+
+	var pick = function(pool) {
+		return shuffle(pool).splice(0, 3);
+	};
+
+	var shuffle = function(a) {
+		var aa = a.slice(0. a.length); // copy
+		for (var i = aa.length - 1; i > 0; i--){
+			var r = Math.floor(Math.random() * (i + 1));
+			var tmp = aa[i];
+			aa[i] = aa[r];
+			aa[r] = tmp;
+		}
+		return aa;
+	};
+
+	document.querySelector('main').addEventListener('click', function() {
+		var result = pick(cards.normal, 3);
+		console.log(result.map(function(c) {return c.name;}));
 	});
 })();
